05b7fbfa627d4f997bea048eb091b5ed288b15e4
chenqi <1549914423@qq.com>
2026-08-21 17:45:11 +0800

    feat:新增修复Coupon、PdfReader、Tree、Barcode、Poster、ShortVideo、ColorPicker、Cropper、Dragsort、Signature、Table2、Title、Cascader组件

--- unpackage/dist/dev/app-harmony/entry/.cxx/default/default/debug/hvigor/arm64-v8a/lib/aarch64-linux-ohos/cmake/uni-app-x-runtime/uni-app-x-runtimeConfig.cmake
+++
@@ -1,5 +0,0 @@
-if(NOT TARGET uni-app-x-runtime::sdk)
-    add_library(uni-app-x-runtime::sdk SHARED IMPORTED)
-    set_target_properties(uni-app-x-runtime::sdk PROPERTIES
-        IMPORTED_LOCATION "/Users/chenqi/Documents/chenqi-front/unibestX/unpackage/dist/dev/app-harmony/oh_modules/.ohpm/@dcloudio+uni-app-x-runtime@hnpaqqbd4i9bxcrp3ktun4a1gqv9ys2o5pljtcetlzk=/oh_modules/@dcloudio/uni-app-x-runtime/libs/arm64-v8a/libsdk.so")
-endif()
