b14c0690b6fe8e3d62ac909dd792fe684e5f0467
cq112233 <cq112233@users.noreply.github.com>
2026-09-25 14:00:06 +0000

    deploy: 0404b8d7210139e114fe9f2e462ed78aa57fe50e

---
+++ assets/index.C6PkuDfX.js
@@ -0,0 +1,1 @@
+import{ad as t}from"./index-Y6Cng3sY.js";function n(n,i="default"){t({title:n,icon:"none"})}function i(n){t({title:n,icon:"success"})}export{i as a,n as t};
---
+++ assets/toast.DLZfmqn0.js
@@ -0,0 +1,1 @@
+import{a7 as t}from"./index-BZkRgI9K.js";function n(n,i="default"){t({title:n,icon:"none"})}function i(n){t({title:n,icon:"success"})}export{i as a,n as t};
